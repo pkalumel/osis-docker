3fdea4c9d944d3148269cacf900690adce8d933b
Patrick KALUME <patrick.kalume@uclouvain.be>
2018-03-01 23:13:04 +0100

    Update databases settings

--- scripts/init-databases.sh
+++ scripts/init-databases.sh
@@ -2,8 +2,15 @@
 set -e
 
 psql -v --username "$POSTGRES_USER" <<-EOSQL
-  CREATE DATABASE osis_backend_dev;
-  CREATE DATABASE osis_front_dev;
-  GRANT ALL PRIVILEGES ON DATABASE osis_backend_dev TO ${POSTGRES_USER};
-  GRANT ALL PRIVILEGES ON DATABASE osis_front_dev TO ${POSTGRES_USER};
+  CREATE DATABASE osis_local;
+  CREATE USER osis WITH PASSWORD 'osis';
+  GRANT ALL PRIVILEGES ON DATABASE osis_local TO osis;
+  REVOKE CONNECT ON DATABASE osis_local FROM public;
+  ALTER USER osis CREATEDB;
+
+  CREATE DATABASE osis_portal_local;
+  CREATE USER osis_portal WITH PASSWORD 'osis';
+  GRANT ALL PRIVILEGES ON DATABASE osis_portal_local TO osis_portal;
+  REVOKE CONNECT ON DATABASE osis_portal_local FROM public;
+  ALTER USER osis_portal CREATEDB;
 EOSQL
